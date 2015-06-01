c2a9d7db538595e4acc86569ceb66bc86e9260d6
Esther Greatbanks <esthergreatbanks@gmail.com>
2015-06-01 18:35:29 +1200

    add and delete from cart working

--- app/controllers/order_lines_controller.rb
+++ app/controllers/order_lines_controller.rb
@@ -1,11 +1,16 @@
 class OrderLinesController < ApplicationController
   before_action :authenticate_user!
 
-  def create(:product_id)
-    OrderLine.create(cart_id: current_user.cart.id, product_id: :product_id)
+  def create
+    product_id = params[:product_id]
+    OrderLine.create(cart_id: current_user.cart.id, product_id: product_id)
+    redirect_to cart_path(current_user.cart.id)
   end
 
   def destroy
+    product_id = params[:product_id]
+    current_user.cart.order_lines.find_by(product_id: product_id).delete
+    redirect_to cart_path(current_user.cart.id)
   end
 
 
